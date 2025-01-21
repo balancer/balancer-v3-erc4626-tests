aec2170f423f29e52fe4b71a8b03ee3d008b4174
João Bruno <joaobrunoah@gmail.com>
2025-01-21 12:19:00 -0300

    Fix liquidity issue

--- test/mainnet/ERC4626MainnetSfrxEth.t.sol
+++ test/mainnet/ERC4626MainnetSfrxEth.t.sol
@@ -20,6 +20,6 @@ contract ERC4626MainnetSFrxEthTest is ERC4626WrapperBaseTest {
         wrapper = IERC4626(0xac3E018457B222d93114458476f3E3416Abbe38F);
         // Donor of frxEth tokens
         underlyingDonor = 0xac3E018457B222d93114458476f3E3416Abbe38F;
-        amountToDonate = 5e4 * 1e18;
+        amountToDonate = 1e4 * 1e18;
     }
 }
