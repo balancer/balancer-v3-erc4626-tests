55513ac4738399a8bd5d9626a912304a3e778746
Matthew Pereira <matthewdavidpereira@gmail.com>
2025-01-29 22:40:14 -0800

    add tests for morpho steakhouse USDQ USDR and USDTL

---
+++ test/mainnet/ERC4626MainnetMorphoSteakhouseUSDR.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import { ERC4626WrapperBaseTest } from "../ERC4626WrapperBase.t.sol";
+
+contract ERC4626MainnetMorphoSteakhouseUsdrTest is ERC4626WrapperBaseTest {
+    function setUp() public override {
+        ERC4626WrapperBaseTest.setUp();
+    }
+
+    function setUpForkTestVariables() internal override {
+        network = "mainnet";
+        overrideBlockNumber = 21735567;
+
+        // Morpho's Steakhouse USDR
+        wrapper = IERC4626(0x30881Baa943777f92DC934d53D3bFdF33382cab3);
+        // Donor of USDR tokens
+        underlyingDonor = 0x77134cbC06cB00b66F4c7e623D5fdBF6777635EC; // Bitfinex Hot Wallet
+        amountToDonate = 1e6 * 1e6;
+    }
+}
