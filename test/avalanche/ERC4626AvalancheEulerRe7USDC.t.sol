0ec1c97189052632e48916adc57ace00d148dc4d
ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
2025-04-17 13:49:49 -0400

    AVAX euler fork tests

---
+++ test/avalanche/ERC4626AvalancheEulerRe7USDC.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import { ERC4626WrapperBaseTest } from "../ERC4626WrapperBase.t.sol";
+
+contract ERC4626AvalancheEulerRe7USDCTest is ERC4626WrapperBaseTest {
+    function setUp() public override {
+        ERC4626WrapperBaseTest.setUp();
+    }
+
+    function setUpForkTestVariables() internal override {
+        network = "avalanche";
+        overrideBlockNumber = 59394164;
+
+        // Eurler USDC
+        wrapper = IERC4626(0x39dE0f00189306062D79eDEC6DcA5bb6bFd108f9);
+        // Donor of USDC tokens
+        underlyingDonor = 0x625E7708f30cA75bfd92586e17077590C60eb4cD;
+        amountToDonate = 10000 * 1e6;
+    }
+}
