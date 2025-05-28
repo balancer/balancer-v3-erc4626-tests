68efd65b6180e52469eb5e63ddc6051b2ecdc904
franz <franzns11@gmail.com>
2025-05-28 13:49:10 +0300

    add stability scusd

--- test/sonic/ERC4626SonicStabilityscUSD.sol
+++ test/sonic/ERC4626SonicStabilityscUSD.sol
@@ -8,7 +8,7 @@ import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import { ERC4626WrapperBaseTest } from "../ERC4626WrapperBase.t.sol";
 
-contract ERC4626SonicStabilityUsdcTest is ERC4626WrapperBaseTest {
+contract ERC4626SonicStabilityscUSDTest is ERC4626WrapperBaseTest {
     function setUp() public override {
         ERC4626WrapperBaseTest.setUp();
     }
@@ -17,10 +17,10 @@ contract ERC4626SonicStabilityUsdcTest is ERC4626WrapperBaseTest {
         network = "sonic";
         overrideBlockNumber = 30141969;
 
-        // Stability USDC
-        wrapper = IERC4626(0xEEEEEEE6d95E55A468D32FeB5d6648754d10A967);
-        // Donor of USDC.e
-        underlyingDonor = 0x578Ee1ca3a8E1b54554Da1Bf7C583506C4CD11c6;
-        amountToDonate = 1e7 * 1e6;
+        // Stability scUSD
+        wrapper = IERC4626(0xccccCCcca9FC69a2b32408730011EdB3205A93A1);
+        // Donor of scUSD
+        underlyingDonor = 0xe6605932e4a686534D19005BB9dB0FBA1F101272;
+        amountToDonate = 1e6 * 1e6;
     }
 }
