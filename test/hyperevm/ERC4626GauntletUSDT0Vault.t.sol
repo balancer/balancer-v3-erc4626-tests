4120db606255e47f9a5d8338bedb6bc83762d7d6
mkflow27 <mkflow27@gmail.com>
2025-07-23 17:12:01 +0200

    test: add gauntletUSDT0

---
+++ test/hyperevm/ERC4626GauntletUSDT0Vault.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import { ERC4626WrapperBaseTest, ERC4626SetupState, ForkState } from "../ERC4626WrapperBase.t.sol";
+
+contract ERC4626GauntletUSDT0Vault is ERC4626WrapperBaseTest {
+    function _setupFork() internal pure override returns (ForkState memory forkState) {
+        // Notice that when executing this function, the fork has not yet been created, so all chain states are empty.
+        forkState.network = "hyperevm";
+        forkState.blockNumber = 9182086;
+    }
+
+    function _setUpForkTestVariables() internal pure override returns (ERC4626SetupState memory erc4626State) {
+        // Hypurrfi's hyUSDT0
+        erc4626State.wrapper = IERC4626(0x53A333e51E96FE288bC9aDd7cdC4B1EAD2CD2FfA);
+
+        // Donor of USDT0
+        erc4626State.underlyingDonor = 0x200000000000000000000000000000000000010C;
+        erc4626State.amountToDonate = 1e6 * 1e6;
+    }
+}
