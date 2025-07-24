10b0563b5577e6aff03e960b658ef9728280cd57
João Bruno <joaobrunoah@gmail.com>
2025-07-24 12:39:23 -0300

    Hyperlend Isolated test

---
+++ test/hyperevm/ERC4626HyperlendIsolatedUSDT0Whlp.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import { ERC4626WrapperBaseTest, ERC4626SetupState, ForkState } from "../ERC4626WrapperBase.t.sol";
+
+contract ERC4626HyperlendIsolatedUSDT0WhlpTest is ERC4626WrapperBaseTest {
+    function _setupFork() internal pure override returns (ForkState memory forkState) {
+        // Notice that when executing this function, the fork has not yet been created, so all chain states are empty.
+        forkState.network = "hyperevm";
+    }
+
+    function _setUpForkTestVariables() internal pure override returns (ERC4626SetupState memory erc4626State) {
+        // Hyperlend's isolated USDT0/WHLP pool
+        erc4626State.wrapper = IERC4626(0x06Fd9D03b3d0F18E4919919b72D30c582f0a97E5);
+
+        // Donor of USDT0
+        erc4626State.underlyingDonor = 0x56aBfaf40F5B7464e9cC8cFF1af13863D6914508;
+        erc4626State.amountToDonate = 1e5 * 1e6;
+    }
+}
