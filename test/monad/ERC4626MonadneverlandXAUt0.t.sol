2aa6d07d458173653104e287dea6910c93933bbc
TLH <94409497+tiago-hansen@users.noreply.github.com>
2026-08-20 13:46:59 -0300

    add fork test for Neverland XAUt0 (#151)
    * add wnXAUt0 fork test
    
    * move failure explanation to PR description

---
+++ test/monad/ERC4626MonadneverlandXAUt0.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import { ERC4626WrapperBaseTest, ERC4626SetupState, ForkState } from "../ERC4626WrapperBase.t.sol";
+
+contract ERC4626MonadneverlandXAUt0Test is ERC4626WrapperBaseTest {
+    function _setupFork() internal pure override returns (ForkState memory forkState) {
+        // Notice that when executing this function, the fork has not yet been created, so all chain states are empty.
+        forkState.network = "monad";
+        forkState.blockNumber = 97149203;
+    }
+
+    function _setUpForkTestVariables() internal pure override returns (ERC4626SetupState memory erc4626State) {
+        // Neverland XAUt0
+        erc4626State.wrapper = IERC4626(0x22139A346b6312EB0A9812C67CfCe4A694676d59);
+        // Donor of XAUt0 tokens
+        erc4626State.underlyingDonor = 0xf89d7b9c864f589bbF53a82105107622B35EaA40;
+        erc4626State.amountToDonate = 5e6;
+        // The XAUt0 market is supply-capped with ~10.75 XAUt0 of headroom, which caps amountToDonate below the
+        // default factor's floor.
+        erc4626State.initToAddFactor = 100;
+    }
+}
